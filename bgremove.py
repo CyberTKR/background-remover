fc00d6da022e3a1b54a9304a009c0fcc333e96ff
TKR <tolg@cybertkr.com>
2023-05-09 10:58:16 +0300

    Create bgremove.py

---
+++ bgremove.py
@@ -0,0 +1,7 @@
+from transparent_image import TransparentImage
+
+image_path = "path/to/image.jpg"
+save_path = "path/to/transparent_image.png"
+
+transparent_image = TransparentImage(image_path)
+transparent_image.make_transparent(save_path)
